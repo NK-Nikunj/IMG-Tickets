ae297bfafc6a97bc8da8391b05ee2d712634fdb6
Nikunj Gupta <nikunj.nkgpta@gmail.com>
2018-07-04 06:11:53 +0530

    Added comment class and referenced it with user and ticket

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Import model to register
-from .models import Ticket
+from .models import Ticket, Comment
 
 
+# Changes in the admin panel for better option availability
 class TicketModelAdmin(admin.ModelAdmin):
 
     # display in the order
@@ -28,7 +29,7 @@ class TicketModelAdmin(admin.ModelAdmin):
 
     # Add search fields
     search_fields = [
-        "question",
+        "issue",
         "content"
     ]
 
@@ -39,5 +40,36 @@ class TicketModelAdmin(admin.ModelAdmin):
 
 
 
+class CommentModelAdmin(admin.ModelAdmin):
+
+    # display in order
+    list_display = [
+        "user",
+        "comment_on",
+        "comment",
+        "updated",
+        "timestamp"
+    ]
+
+    # Filter list tags
+    list_filter = [
+        "user",
+        "comment_on",
+        "updated",
+        "timestamp"
+    ]
+
+    # Add search fields
+    search_fields = [
+        "comment"
+    ]
+
+
+    # Meta class to associate to model Comment
+    class Meta:
+        model = Comment
+
+
 # Register with admin
 admin.site.register(Ticket, TicketModelAdmin)
+admin.site.register(Comment, CommentModelAdmin)
--- tickets/models.py
+++ tickets/models.py
@@ -10,6 +10,7 @@ def upload_location(instance, filename):
     return "%s/%s" %(instance.id, filename)
 
 
+# Ticket model for the ticketing app
 class Ticket(models.Model):
 
     """
@@ -28,10 +29,15 @@ class Ticket(models.Model):
          -> Resolved. In case an issue is infeasible/non-reasonable then staff
          can assign it a state of 'not possible'. States can ONLY be assigned by
          staff.
+    [8]: User has the right to edit/delete his/her post at any time. Doing so
+         will update time accordingly. For this reason this model has 2
+         DateTimeField instead of 1.
 
         TODO:
-    [8]: Staff can assign the ticket to a staff member. The staff member it is
+    [1]: Staff can assign the ticket to a staff member. The staff member it is
          assinged to can then accept or reject it.
+    [2]: Show edit history of ticket raised so that others can look into
+         previous versions of the ticket.
     """
 
     # choice for tags
@@ -57,21 +63,27 @@ class Ticket(models.Model):
     )
 
     # Model fields starts here
+
+    # Reference to User on Ticket creation
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE
     )
 
+    # Tag for ticket
     tag = models.CharField(
         max_length=30,
         choices=TAGS,
         default='BUG'
     )
 
+    # Issue that is bothering
     issue = models.CharField(max_length=300)
 
+    # Brief expanation of the issue
     content = models.TextField()
 
+    # Image reference to the issue
     image = models.ImageField(
         upload_to=upload_location,
         null=True,
@@ -83,33 +95,93 @@ class Ticket(models.Model):
 
     width_field = models.IntegerField(default=0)
 
+    # Updated timestamp for Ticket
     updated = models.DateTimeField(
         auto_now=True,
         auto_now_add=False
     )
 
+    # Created timestamp for Ticket
     timestamp = models.DateTimeField(
         auto_now=False,
         auto_now_add=True
     )
 
+    # Status of Ticket
     ticket_state = models.CharField(
         max_length=10,
         choices=TICKET_STATE,
         default='PUB'
     )
 
+    # Completion state of Ticket
     completion_state = models.CharField(
         max_length=30,
         choices=COMPLETION_STATE,
         default='REC'
     )
 
+
     # Meta class to determine the order sequence of tickets
     class Meta:
-        ordering = ["-updated", "-timestamp"]
+        ordering = [
+            "-updated",
+            "-timestamp"
+        ]
 
 
     # Class string as the issue name of ticket
     def __str__(self):
         return self.issue
+
+
+
+# Comments model for the ticketing app
+class Comment(models.Model):
+
+    """
+    Comment feature provides the following features to the User and Staff:
+
+    [1]: Creating a comment references itself with User and Ticket raised.
+    [2]: User/Staff can create/edit/delete comments. In addition,
+         Staff can delete comments of other users provided they are not
+         staff as well.
+
+        TODO:
+    [1]: Provide version history of edited comments for better understanding.
+    [2]: Provide a way to reference one comment with some other ticket
+         (something similar to GitHub's reference system).
+    """
+
+    # Ticket reference to comment
+    comment_on = models.ForeignKey(
+        'tickets.Ticket',
+        on_delete=models.CASCADE,
+    )
+
+    # User reference to comment
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    # Comment for the ticket
+    comment = models.TextField()
+
+    # Updated timestamp for Comment
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    # Created at timestamp for Comment
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+
+    # Meta class for ordering
+    class Meta:
+        ordering = [
+            "-updated",
+            "-timestamp"
+        ]
+
+    # Class string as the comment iself
+    def __str__(self):
+        return self.comment
